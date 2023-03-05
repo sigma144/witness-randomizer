2e0eb55d1a2fd27c6b821579e43b5ad58d40099f
blastron <blastron@mac.com>
2023-03-05 13:25:30 -0800

    Multiple text blocks.

--- Source/DataTypes.h
+++ Source/DataTypes.h
@@ -1,9 +1,19 @@
 #pragma once
 
 struct RgbColor {
-	RgbColor() : R(1.f), G(1.f), B(1.f) {}
-	RgbColor(float R, float G, float B) : R(R), G(G), B(B) {}
+	RgbColor() : R(1.f), G(1.f), B(1.f), A(1.f) {}
+	RgbColor(float R, float G, float B) : R(R), G(G), B(B), A(1.f) {}
+	RgbColor(float R, float G, float B, float A) : R(R), G(G), B(B), A(A) {}
+
 	float R;
 	float G;
 	float B;
+	float A;
+
+	int argb() const {
+		return ((int)(A * 255.5) << 24) |
+			((int)(R * 255.5) << 16) |
+			((int)(G * 255.5) << 8) |
+			((int)(B * 255.5));
+	}
 };
--- Source/HUDManager.cpp
+++ Source/HUDManager.cpp
@@ -271,50 +271,95 @@ void HudManager::overwriteSubtitleFunction() {
 	uint32_t screen_target_height = 0x469A5F4;
 
 	// TODO: find a sane size for this
-	uint64_t dataPayload = (uint64_t)VirtualAllocEx(memory->_handle, NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	uint64_t address_payload = (uint64_t)VirtualAllocEx(memory->_handle, NULL, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-	uint64_t address_tempLineCount = dataPayload;
-	uint64_t address_tempHorizAlignment = dataPayload + 0x08;
-	uint64_t address_tempVerticalAlignment = dataPayload + 0x10;
-	uint64_t address_tempString = dataPayload + 0x18;
+	//uint64_t address_tempLineCount = dataPayload;
+	//uint64_t address_tempHorizAlignment = dataPayload + 0x08;
+	//uint64_t address_tempVerticalAlignment = dataPayload + 0x10;
+	//uint64_t address_tempString = dataPayload + 0x18;
 
-	const float margin_width = 0.f;//0.02f;
+	uint32_t payloadInfo_address_blockCount = 0x0;
+	uint32_t payloadInfo_totalSize = 0x8;
+
+	uint32_t blockInfo_address_lineCount = 0x0;
+	uint32_t blockInfo_address_horizontalAlignment = 0x08;
+	uint32_t blockInfo_address_horizontalPosition = 0x10;
+	uint32_t blockInfo_address_vertPosition = 0x18;
+	uint32_t blockInfo_totalSize = 0x20;
+
+	uint32_t lineInfo_address_color = 0x0;
+	uint32_t lineInfo_address_string = 0x8;
+	uint32_t lineInfo_totalSize = 0x8 + SUBTITLE_MAX_LINE_LENGTH;
+
+	const float margin_width = 0.02f;
 	const float line_spacing = 1.0f;
+	const float line_top_adjust = 0.25f;
 
-	uint32_t tempLineCount = 4;
-	WriteProcessMemory(memory->_handle, (LPVOID)address_tempLineCount, &tempLineCount, sizeof(uint32_t), NULL);
+	uint64_t temp_writer = address_payload;
 
-	float tempHorizAlignment = 0.5f;
-	WriteProcessMemory(memory->_handle, (LPVOID)address_tempHorizAlignment, &tempHorizAlignment, sizeof(float), NULL);
+	// payload info
+	uint32_t tempBlockCount = 3;
+	WriteProcessMemory(memory->_handle, (LPVOID)(temp_writer + payloadInfo_address_blockCount), &tempBlockCount, sizeof(uint32_t), NULL);
+	temp_writer += payloadInfo_totalSize;
 
-	float tempVerticalAlignment = 0.5f;
-	WriteProcessMemory(memory->_handle, (LPVOID)address_tempVerticalAlignment, &tempVerticalAlignment, sizeof(float), NULL);
+	// block info
+	for (int blockId = 0; blockId < tempBlockCount; blockId++) {
+		uint32_t tempLineCount = 4;
+		WriteProcessMemory(memory->_handle, (LPVOID)(temp_writer + blockInfo_address_lineCount), &tempLineCount, sizeof(uint32_t), NULL);
 
-	char tempString1[] = "i am, at this moment...";
-	char tempString2[] = "oh, what do the kids say these days?";
-	char tempString3[] = "\"pogged out of my gourd\"";
-	char tempString4[] = "over centering some text";
+		float tempHorizAlignment = blockId * 0.5f;
+		WriteProcessMemory(memory->_handle, (LPVOID)(temp_writer + blockInfo_address_horizontalAlignment), &tempHorizAlignment, sizeof(float), NULL);
 
-	uint32_t line_data_length = SUBTITLE_MAX_LINE_LENGTH;
+		float tempHorizPosition = blockId * 0.5f;
+		WriteProcessMemory(memory->_handle, (LPVOID)(temp_writer + blockInfo_address_horizontalPosition), &tempHorizPosition, sizeof(float), NULL);
 
-	WriteProcessMemory(memory->_handle, (LPVOID)(address_tempString + line_data_length * 0), tempString4, sizeof(tempString4), NULL);
-	WriteProcessMemory(memory->_handle, (LPVOID)(address_tempString + line_data_length * 1), tempString3, sizeof(tempString3), NULL);
-	WriteProcessMemory(memory->_handle, (LPVOID)(address_tempString + line_data_length * 2), tempString2, sizeof(tempString2), NULL);
-	WriteProcessMemory(memory->_handle, (LPVOID)(address_tempString + line_data_length * 3), tempString1, sizeof(tempString1), NULL);
+		float tempVerticalAlignment = blockId * 0.5f;
+		WriteProcessMemory(memory->_handle, (LPVOID)(temp_writer + blockInfo_address_vertPosition), &tempVerticalAlignment, sizeof(float), NULL);
+
+		temp_writer += blockInfo_totalSize;
+
+		for (int lineId = 0; lineId < tempLineCount; lineId++) {
+			RgbColor tempColor(1.f,tempHorizAlignment,lineId*0.3f,0.5f+lineId*0.15f);
+			uint32_t tempArgb = tempColor.argb();
+			WriteProcessMemory(memory->_handle, (LPVOID)(temp_writer + lineInfo_address_color), &tempArgb, sizeof(uint32_t), NULL);
+
+			std::stringstream lineText;
+			lineText << "BLOCK: " << blockId + 1 << ", string: " << (lineId + 1);
+
+			char stringBuff[SUBTITLE_MAX_LINE_LENGTH];
+			strcpy_s(stringBuff, lineText.str().c_str());
+			WriteProcessMemory(memory->_handle, (LPVOID)(temp_writer + lineInfo_address_string), stringBuff, sizeof(stringBuff), NULL);
+
+			temp_writer += lineInfo_totalSize;
+		}
+	}
 
 ////// BEGIN REPLACEMENT FUNCTION
+
+	// The data structure:
+	//  - int32 num_blocks
+	//  - block[] blocks:
+	//    - int32 num_lines
+	//    - float horiz_alignment
+	//    - float vert_alignment
+	//    - line[] lines: (packed from last to first)
+	//      - int32 packed_color
+	//      - char[] string
 
 	AsmBuilder func(func_hud_draw_subtitles);
 
 	const uint32_t stack_cache_RBX = 0xB8;
 	const uint32_t stack_cache_R12 = 0xB0;
 	const uint32_t stack_cache_R13 = 0xA8;
+	const uint32_t stack_cache_R14 = 0xA0;
 
-	const uint32_t stack_y_origin = 0xA0;
-	const uint32_t stack_line_height = 0x9A;
-	const uint32_t stack_margin_pixels = 0x90;
-	const uint32_t stack_draw_area_x = 0x88;
-	const uint32_t stack_draw_area_y = 0x80;
+	const uint32_t stack_y_origin = 0x9A;
+	const uint32_t stack_line_height = 0x90;
+	const uint32_t stack_margin_pixels = 0x88;
+	const uint32_t stack_draw_area_x = 0x80;
+	const uint32_t stack_draw_area_y = 0x78;
+	const uint32_t stack_currentBlock_horizAlignment = 0x70;
+	const uint32_t stack_currentBlock_horizPosition = 0x68;
 
 	////////////
 	// Save off registers to the stack.
@@ -326,7 +371,9 @@ void HudManager::overwriteSubtitleFunction() {
 		.add({ 0x4C,0x89,0xA4,0x24 })		// MOV qword ptr [RSP + stack_cache_R12], R12
 		.val(stack_cache_R12)
 		.add({ 0x4C,0x89,0xAC,0x24 })		// MOV qword ptr [RSP + stack_cache_R13], R13
-		.val(stack_cache_R13);
+		.val(stack_cache_R13)
+		.add({ 0x4C,0x89,0xB4,0x24 })		// MOV qword ptr [RSP + stack_cache_R14], R14
+		.val(stack_cache_R14);
 	////////////
 
 	////////////
@@ -347,16 +394,6 @@ void HudManager::overwriteSubtitleFunction() {
 	//     ECX				vertsPerPrimitive
 	func.add({ 0xB9,0x03,0x00,0x00,0x00 })	// MOV ECX, 0x3
 		.add({ 0xE8 }).rel(func_im_begin);	// CALL <im_begin>
-	////////////
-
-	////////////
-	// Load the line count into a stable register.
-	// Output:
-	//     R12D:4			int32 line_count
-	////////////
-	func.add({ 0x49,0xBC })					// MOVABS R12, <line_count>
-		.abs(address_tempLineCount)
-		.add({ 0x45,0x8B,0x24,0x24 });		// MOV R12D, dword ptr [R12]
 	////////////
 
 	////////////
@@ -398,11 +435,73 @@ void HudManager::overwriteSubtitleFunction() {
 	////////////
 
 	////////////
+	// Load data about the overall set of blocks into stable registers.
+	// Output:
+	//     R13				void* read_head
+	//     R14D:4			int32 block_count
+	////////////
+	// - Load the address of the payload into R13, which we will use as a read head as we move through our data.
+	func.add({ 0x49,0xBD })					// MOVABS R13, <address_payload>
+		.abs(address_payload)
+	// - Load the number of blocks into R14.
+		.add({ 0x45,0x8B,0xB5 })			// MOV R14D, dword ptr [R13 + payloadInfo_address_blockCount]
+		.val(payloadInfo_address_blockCount)
+	// - Move the read head forward by the total payload size.
+		.add({ 0x49,0x81,0xC5 })			// ADD R13, payloadInfo_totalSize
+		.val(payloadInfo_totalSize);
+	////////////
+
+	////////////
+	// Start the block loop.
+	////////////
+	int blockLoopStart = func.size();
+	////////////
+
+	////////////
+	// Decrement the block counter.
+	////////////
+	func.add({ 0x41,0xFF,0xCE });			// DEC R14D
+	////////////
+	
+	////////////
+	// Load the line count into a stable register.
+	// Input:
+	//     R13				void* read_head
+	// Output:
+	//     R12D:4			int32 line_count
+	////////////
+	func.add({ 0x45,0x8B,0xA5 })			// MOV R12D, dword ptr [R13 + blockInfo_address_lineCount]
+		.val(blockInfo_address_lineCount);
+	////////////
+	
+	////////////
+	// Save off the horizontal position and alignment parameters for the block.
+	// Input:
+	//     R13				void* read_head
+	////////////
+	// - Save the horizontal alignment.
+	//     R8D:4			(scratch for transferring)
+	func.add({ 0x45,0x8B,0x85 })			// MOV R8D, dword ptr [r13 + blockInfo_address_horizontalAlignment]
+		.val(blockInfo_address_horizontalAlignment)
+		.add({ 0x44,0x89,0x84,0x24 })		// MOV dword ptr [rsp + stack_currentBlock_horizAlignment], R8D
+		.val(stack_currentBlock_horizAlignment);
+	// - Save the horizontal position.
+	//     R8D:4			(scratch for transferring)
+	func.add({ 0x45,0x8B,0x85 })			// MOV R8D, dword ptr [r13 + blockInfo_address_horizontalPosition]
+		.val(blockInfo_address_horizontalPosition)
+		.add({ 0x44,0x89,0x84,0x24 })		// MOV dword ptr [rsp + stack_currentBlock_horizPosition], R8D
+		.val(stack_currentBlock_horizPosition);
+	////////////
+
+	////////////
 	// Compute the Y origin of the entire block of text based on its line count.
 	// Input:
 	//     R12D:4			int32 line_count
-	//     XMM0_Da:4		float draw_area_y
+	//     R13				void* read_head
 	////////////
+	// - Load the drawable area of the screen.
+	func.add({ 0xF3,0x0F,0x10,0x84,0x24 })	// MOVSS XMM0, dword ptr [RSP + stack_draw_area_y]
+		.val(stack_draw_area_y);
 	// - Retrieve our font and find its line height.
 	//     R8:8				Dynamic_Font* font
 	//     XMM1_Da:4		float font::height
@@ -432,17 +531,23 @@ void HudManager::overwriteSubtitleFunction() {
 	// - Add the height of an un-spaced line to get the total height of the block.
 	//     XMM3_Da:4		float block_height
 	func.add({ 0xF3,0x0F,0x58,0xD9 });		// ADDSS XMM3, XMM1
+	// - Next, remove a fraction of a line's worth of text from the block in order to compensate for the fact that the
+	//   font's height already has spacing built-in.
+	//     XMM2_Da:4		float adjustment
+	//     XMM3_Da:4		float block_height
+	func.add({ 0x41,0xB8 })					// MOV R8D, <line_top_adjust>
+		.val(line_top_adjust)
+		.add({ 0x66,0x41,0x0F,0x6E,0xD0 })	// MOVD XMM2, R8D
+		.add({ 0xF3,0x0F,0x59,0xD1 })		// MULSS XMM2, XMM1
+		.add({ 0xF3,0x0F,0x5C,0xDA });		// SUBSS XMM3, XMM2
 	// - Subtract the block height from the drawable area of the screen to find the relative position of the block if it
-	//   were to be placed at the top of the screen, then multiply it by the vertical alignment parameter in order to
+	//   were to be placed at the top of the screen, then multiply it by the vertical positioning parameter in order to
 	//   place it at the correct height. Our formula is: [(draw_area_y - block_height) * vertical_alignment]
-	//     R8:8				float* vertical_alignment
 	//     XMM1_Da:4		float vertical_alignment
 	//     XMM0_Da:4		float relative_position
 	func.add({ 0xF3,0x0F,0x5C,0xC3 })		// SUBSS XMM0, XMM3
-		.add({ 0x49,0xB8 })					// MOVABS R8, <vertical alignment address>
-		.abs(address_tempVerticalAlignment)
-		.add({ 0x45,0x8B,0x00 })			// MOV R8D, dword ptr [R8]
-		.add({ 0x66,0x41,0x0F,0x6E,0xC8 })	// MOVD XMM1, R8D
+		.add({ 0x66,0x41,0x0F,0x6E,0x8D })	// MOVD XMM1, [R13 + blockInfo_address_vertPosition]
+		.val(blockInfo_address_vertPosition)
 		.add({ 0xF3,0x0F,0x59,0xC1 });		// MULSS XMM0, XMM1
 	// - Load the margin, add it to the relative position in order to find the final absolute position, then save it off.
 	//     XMM0_Da:4		float absolute_position
@@ -455,35 +560,23 @@ void HudManager::overwriteSubtitleFunction() {
 	////////////
 
 	////////////
+	// Advance the read head to past the block info.
+	////////////
+	func.add({ 0x49,0x81,0xC5 })			// ADD R13, blockInfo_totalSize
+		.val(blockInfo_totalSize);
+	////////////
+
+	////////////
 	// Start the line loop.
 	// NOTE: R12D goes from being line_count to current_index here.
 	////////////
-	int loopStart = func.size();
+	int lineLoopStart = func.size();
 	////////////
 
 	////////////
-	// Decrement the counter so that we're pointing at the next string.
+	// Decrement the line counter.
 	////////////
 	func.add({ 0x41,0xFF,0xCC });			// DEC R12D
-	////////////
-
-	////////////
-	// Compute the address of our current string.
-	// Input:
-	//     R12D:4			int32 current_index
-	// Output:
-	//     R13:8			char* line_address
-	////////////
-	// - Get the address of the start of our buffer.
-	//     R13:8			char* root_address
-	func.add({ 0x49, 0xBD })				// MOV R13, <root_address>
-		.abs(address_tempString);
-	// - Adjust the string pointer by an offset based on the current index.
-	//     R11D:4			int32 line_data_length
-	func.add({ 0x41,0xBB })					// MOV R11D, <line_data_length>
-		.val(line_data_length)
-		.add({ 0x45,0x0F,0xAF,0xDC })		// IMUL R11D, R12D			<-- multiply line length by loop count to get target address offset
-		.add({ 0x4D,0x01,0xDD });			// ADD R13, R11				<-- add the address offset. note that it's safe to go from a 32->64 bit int here, since the upper half of a numbered register is zeroed
 	////////////
 
 	////////////
@@ -493,7 +586,7 @@ void HudManager::overwriteSubtitleFunction() {
 	//     RDX:8			char* string
 	//     R8D:4			int32 flags		<-- always 0x20 as far as i can tell
 	// Input:
-	//     R13:8			char* line_address
+	//     R13:8			void* read_head
 	// Output:
 	//     XMM0_Da:4		float string_width
 	////////////
@@ -501,7 +594,9 @@ void HudManager::overwriteSubtitleFunction() {
 	func.add({ 0x48,0x8B,0x0D })			// MOV RCX, qword ptr [RIP + <font>]
 		.rel(global_subtitle_font);
 	// - Set string pointer
-	func.add({ 0x4C,0x89,0xEA });			// MOV RDX, R13 
+	func.add({ 0x4C,0x89,0xEA })			// MOV RDX, R13
+		.add({ 0x48,0x81,0xC2 })			// ADD RDX, lineInfo_address_string
+		.val(lineInfo_address_string);
 	// - Set flags
 	func.add({ 0x41,0xB8 })					// MOV R8D, 0x20
 		.val<uint32_t>(0x20);
@@ -512,27 +607,29 @@ void HudManager::overwriteSubtitleFunction() {
 
 	////////////
 	// Determine the X origin of the string based on its width and the horizontal alignment parameter. The formula here is:
-	//   [(draw_area_x - string_width) * horizontal_alignment]
+	//   [(draw_area_x * horizontal_position) - (string_width * horizontal_alignment)]
 	// Input:
 	//     XMM0_Da:4		float string_width
 	// Output:
 	//     XMM1_Da:4		float xPos
 	////////////
-	// - Retrieve the drawable area, then subtract the string width from it in order to get the relative position of the string
-	//   in the drawable area, were it to be aligned all the way to the right.
-	//     XMM1_Da:4		float relative_position
-	func.add({ 0xF3,0x0F,0x10,0x8C,0x24 })	// MOVSS XMM1, dword ptr [RSP + stack_draw_area_x]
+	// - Multiply the string width by the horizontal alignment parameter in order to get its offset from the origin.
+	//     XMM0_Da:4		float string_offset
+	func.add({ 0x66,0x0F,0x6E,0x8C,0x24 })	// MOVD XMM1, dword ptr [RSP + stack_currentBlock_horizPosition]
+		.val(stack_currentBlock_horizPosition)
+		.add({ 0xF3,0x0F,0x59,0xC1 });		// MULSS XMM0, XMM1
+	// - Retrieve the drawable area, then multiply it by the horizontal positioning parameter in order to get the origin
+	//   of the string.
+	//     XMM2_Da:4		float horizontal_alignment
+	//     XMM1_Da:4		float string_origin
+	func.add({ 0x66,0x0F,0x6E,0x94,0x24 })	// MOVD XMM2, dword ptr [RSP + stack_currentBlock_horizAlignment]
+		.val(stack_currentBlock_horizAlignment)
+		.add({ 0xF3,0x0F,0x10,0x8C,0x24 })	// MOVSS XMM1, dword ptr [RSP + stack_draw_area_x]
 		.val(stack_draw_area_x)
-		.add({ 0xF3,0x0F,0x5C,0xC8 });		// SUBSS XMM1, XMM0
-	// - Multiply the relative position by the horizontal alignment parameter.
-	//     R8:8				float* horizontal_alignment
-	//     XMM0_Da:4		float horizontal_alignment
+		.add({ 0xF3,0x0F,0x59,0xCA });		// MULSS XMM1, XMM2
+	// - Subtract the offset from the origin to get the position of the string relative to the drawable area.
 	//     XMM1_Da:4		float relative_position
-	func.add({ 0x49,0xB8 })					// MOVABS R8, <horizontal alignment address>
-		.abs(address_tempHorizAlignment)
-		.add({ 0x45,0x8B,0x00 })			// MOV R8D, dword ptr [R8]
-		.add({ 0x66,0x41,0x0F,0x6E,0xC0 })	// MOVD XMM0, R8D
-		.add({ 0xF3,0x0F,0x59,0xC8 });		// MULSS XMM1, XMM0
+	func.add({ 0xF3,0x0F,0x5C,0xC8 });		// SUBSS XMM1, XMM0
 	// - Load the margin, then add it to the relative position in order to find the final absolute position.
 	//     XMM0_Da:4		float margin
 	//     XMM1_Da:4		float xPos
@@ -545,7 +642,7 @@ void HudManager::overwriteSubtitleFunction() {
 	// Input:
 	//     R12D:4			int32 current_index
 	// Output:
-	//     XMM2_Da:4
+	//     XMM2_Da:4		float yPos
 	////////////
 	// - Convert the loop counter to a float.
 	//     XMM2_Da:4		float line_count
@@ -566,7 +663,7 @@ void HudManager::overwriteSubtitleFunction() {
 	////////////
 	// Set parameters and call draw_text.
 	// Input:
-	//     R13:8			char* string
+	//     R13:8			void* read_head
 	//     XMM1_Da:4		float xPos
 	//     XMM2_Da:4		float yPos
 	// draw_text parameters:
@@ -581,24 +678,41 @@ void HudManager::overwriteSubtitleFunction() {
 	func.add({ 0x48,0x8B,0x0D })			// MOV RCX, qword ptr [RIP + <font>]
 		.rel(global_subtitle_font);
 	// - Set color.
-	func.add({ 0x41, 0xB9 })				// MOV R9D, 0xFFFFFFFF
-		.add({ 0xFF, 0xFF, 0xFF, 0xFF });
+	func.add({ 0x45,0x8B,0x8D })			// MOV R9D, dword ptr [R13 + lineInfo_address_color]
+		.val(lineInfo_address_color);
 	// - Set string pointer
-	func.add({ 0x4C, 0x89, 0x6C, 0x24, 0x20 });	// MOV qword ptr [RSP + 0x20], R13
+	func.add({ 0x4D,0x89,0xE8 })			// MOV R8, R13
+		.add({ 0x49,0x81,0xC0 })			// ADD R8, lineInfo_address_string
+		.val(lineInfo_address_string)
+		.add({ 0x4C,0x89,0x44,0x24,0x20 });	// MOV qword ptr [RSP + 0x20], R8
 	// - Set flags
 	func.add({ 0xC7, 0x44, 0x24, 0x28 })	// MOV dword ptr [RSP + 0x28], 0x20
 		.val<uint32_t>(0x20);
 	// - Call draw_text
 	func.add({ 0xE8 }).rel(func_draw_text);	// CALL <draw_text>
 	////////////
+	
+	////////////
+	// Advance the read head to past the line info.
+	////////////
+	func.add({ 0x49,0x81,0xC5 })			// ADD R13, lineInfo_totalSize
+		.val(lineInfo_totalSize);
+	////////////
 
 	////////////
-	// Jump back in the loop if we haven't reached the end.
+	// Jump back in the line loop if we haven't reached the end.
 	////////////
 	func.add({ 0x45,0x85,0xE4 });			// TEST R12D,R12D
-	int loopEnd = func.size();
 	func.add({ 0x0F,0x85 })					// JNE <loopStart>
-		.val((loopStart - (loopEnd + 6)));
+		.val((lineLoopStart - (func.size() + 4)));
+	////////////
+
+	////////////
+	// Jump back in the line loop if we haven't reached the end.
+	////////////
+	func.add({ 0x45,0x85,0xF6 });			// TEST R14D,R14D
+	func.add({ 0x0F,0x85 })					// JNE <loopStart>
+		.val((blockLoopStart - (func.size() + 4)));
 	////////////
 
 	////////////
@@ -616,7 +730,9 @@ void HudManager::overwriteSubtitleFunction() {
 		.val(stack_cache_R12)
 		.add({ 0x4C,0x8B,0xAC,0x24 })		// MOV R13, qword ptr [RSP + stack_cache_R13]
 		.val(stack_cache_R13)
-		.add({ 0x48,0x81,0xC4 })			// ADD RSP,0xD8
+		.add({ 0x4C,0x8B,0xB4,0x24 })		// MOV R14, qword ptr [RSP + stack_cache_R14]
+		.val(stack_cache_R14)
+		.add({ 0x48,0x81,0xC4 })			// ADD RSP, 0xD8
 		.add({ 0xD8,0x00,0x00,0x00 });
 	////////////
 
