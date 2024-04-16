2cdade473d63d28594cbbc670fa9b190a1d923f5
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-04-17 01:11:22 +0200

    bump version

--- App/Version.h
+++ App/Version.h
@@ -7,10 +7,10 @@
 #define MINOR 0
 #define PATCH 0
 
-#define VERSION_STR     "5.0.2 Error Logging 2"
+#define VERSION_STR     "5.0.3"
 #define VERSION			MAJOR, MINOR, PATCH
 
-#define AP_VERSION_STR	"0.4.5"
+#define AP_VERSION_STR	"0.4.6"
 #define AP_VERSION_STR_BACKCOMPAT  "0.4.4"
 
 #define PRODUCT_NAME L"Witness Random Puzzle Generator for Archipelago.gg"
