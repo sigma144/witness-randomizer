5b7f3de84805469a558b8a66071ba87e8e479387
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-05-01 01:12:13 +0200

    Nvm can't do that

--- Source/Memory.cpp
+++ Source/Memory.cpp
@@ -29,6 +29,7 @@ Memory::Memory() {
 	PROCESSENTRY32 entry;
 	entry.dwSize = sizeof(entry);
 	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	bool supposedlyFound = false;
 	bool foundAnyProcesses = false;
 
 	std::ofstream outputStream = std::ofstream("FindProcessErrorLog.txt");
@@ -39,13 +40,9 @@ Memory::Memory() {
 
 		foundAnyProcesses = true;
 		if (entry.szExeFile == process64) {
+			supposedlyFound = true;
 			_handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
 			break;
-
-			if (!_handle) {
-				MessageBox(GetActiveWindow(), L"Found Witness exe but could not retrieve handle to it. Is this program being quarantined by an antivirus?", NULL, MB_OK);
-				throw std::exception("Found Witness exe but could not retrieve handle to it.");
-			}
 		}
 	}
 
@@ -58,6 +55,11 @@ Memory::Memory() {
 
 	// If we didn't find the process, terminate.
 	if (!_handle) {
+		if (supposedlyFound) {
+			MessageBox(GetActiveWindow(), L"Found Witness exe but could not retrieve handle to it. Is this program being quarantined by an antivirus?", NULL, MB_OK);
+			throw std::exception("Found Witness exe but could not retrieve handle to it.");
+		}
+
 		PROCESSENTRY32 entry;
 		entry.dwSize = sizeof(entry);
 		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
