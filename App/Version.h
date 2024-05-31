847ebff6a200a4d7af87f3b9d3412f7d43da5d2d
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-05-31 23:07:47 +0200

    Version bump

--- App/Version.h
+++ App/Version.h
@@ -7,7 +7,7 @@
 #define MINOR 0
 #define PATCH 0
 
-#define VERSION_STR     "6.0.0 Beta 3"
+#define VERSION_STR     "6.0.0 Beta 4"
 #define VERSION			MAJOR, MINOR, PATCH
 
 #define AP_VERSION_STR	"0.4.7"
