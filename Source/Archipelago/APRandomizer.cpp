f445a2d5ed16dd58db5c0efbc78ccb5940f7b24e
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-09-01 15:18:23 +0200

    Why was this here?

--- Source/Archipelago/APRandomizer.cpp
+++ Source/Archipelago/APRandomizer.cpp
@@ -680,7 +680,6 @@ void APRandomizer::PostGeneration() {
 	if (FinalPanel == 0x03629) {
 		Special::writeGoalCondition(0x0042D, " Goal:", "Panel Hunt", " Lasers:", MountainLasers, ChallengeLasers);
 		Special::DrawSingleVerticalLine(0x03629);
-		memory->WritePanelData<float>(0x03629, MAX_BROADCAST_DISTANCE, -1);
 		Special::DrawSingleVerticalLine(0x03505);
 	}
 	else if (FinalPanel == 0x09F7F) {
