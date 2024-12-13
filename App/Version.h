905ddbda0db91a033eed882d7a2474ae632ae016
NewSoupVi <57900059+NewSoupVi@users.noreply.github.com>
2024-12-13 11:00:07 +0100

    Variety PP Fix

--- App/Version.h
+++ App/Version.h
@@ -5,9 +5,9 @@
 
 #define MAJOR 7
 #define MINOR 0
-#define PATCH 0
+#define PATCH 1
 
-#define VERSION_STR     "7.0.0 Extra Logging"
+#define VERSION_STR     "7.0.1"
 #define VERSION			MAJOR, MINOR, PATCH
 
 #define AP_VERSION_STR	"0.5.1"
